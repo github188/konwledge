1e5c1220bcdc00522b75ed0960ae602d68c675c3
Mr.Gao <gaofangshang9868@163.com>
2018-02-03 15:12:48 +0800

    de

--- 源生封装/js函数封装/demo学习.js
+++ 源生封装/js函数封装/demo学习.js
@@ -12,3 +12,35 @@ Object.prototype.addListener=function (type, fn, boolean) {
         videoStart.attachEvent(type,fn)
     }
 };
+Array.prototype.remove = function(element){
+
+    for (var i = 0,j=0; i < this.length; i++) {
+        if(this[i]!=element){
+            this[j++]=this[i];
+        }
+    }
+    this.length-=1
+}
+//删除元素下标
+Array.prototype.removeIndex=function(dx)
+{
+    if(isNaN(dx)||dx>this.length)
+    {
+        alert("删除对象下标越界");
+        return false;
+    }
+    for(var k=0,n=0;k<this.length;k++)
+    {
+        if(this[k]!=this[dx])
+        {
+            this[n++]=this[k];
+        }
+    }
+    this.length-=1
+}
+
+var arr =["Iphone","MI","SAMSUNG","VIVO","OPPO","HUAWEI","ZTE","SONY","Coolpad","GiONEE","MEIZU","nubia","Letv","TCL"];
+arr.push("NOKIA");
+arr.remove("Iphone");
+arr.removeIndex(1);
+alert(arr.join(","));
