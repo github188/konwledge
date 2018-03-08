051a5d7763c4f84caaebdedcc4deba71480eb155
Mr.Gao <gaofangshang9868@163.com>
2018-03-08 18:23:03 +0800

    node

---
+++ node/express/demo1.js
@@ -0,0 +1,16 @@
+//express_demo.js 文件
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+    console.log(server.address())
+
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)//http://127.0.0.1:8081/
+})
