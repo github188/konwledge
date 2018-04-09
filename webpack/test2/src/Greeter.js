23055093c1df8a541905521cf64d1482364a4585
Mr.Gao <gaofangshang9868@163.com>
2018-04-09 17:45:42 +0800

    tempalte

--- webpack/test2/src/Greeter.js
+++ webpack/test2/src/Greeter.js
@@ -1,5 +1,33 @@
-module.exports = function() {
-    var greet = document.createElement('div');
-    greet.textContent = "Hi there and greetings!";
-    return greet;
-};
+// var config = require('./config.json');
+// module.exports = function() {
+//     var greet = document.createElement('div');
+//     greet.textContent = config.greetText;
+//     return greet;
+// };
+
+//Greeter,js
+import React, {Component} from 'react'
+import config from './config.json';
+import styles from './Greeter.css';//导入
+import styles2 from './test.scss'
+
+class Greeter extends Component{
+    render() {
+        return (
+            <div>
+                <div className={styles.root}>
+                    {config.greetText}
+                </div>
+                <div className={styles.box}>
+                    {config.testText}
+                </div>
+                <div className={styles2.box2}>
+                    {config.sassText}
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Greeter
--- webpack/test2/webpack.production.config.js
+++ webpack/test2/webpack.production.config.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     devtool:"eval-source-map",
@@ -43,7 +45,7 @@ module.exports = {
         ]
     },
     devServer: {
-       // contentBase: "./dist",//默认webpack-dev-server会为根文件夹提供本地服务器;如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录
+        // contentBase: "./dist",//默认webpack-dev-server会为根文件夹提供本地服务器;如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录
         historyApiFallback: true,//在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
         inline: true,//设置为true，当源文件改变时会自动刷新页面
         port:8008,//设置默认监听端口，如果省略，默认为”8080“
@@ -56,6 +58,16 @@ module.exports = {
             filename: "index.html", //在目标文件中，生成的html文件名
             hash: true,         //是否
         }),
-        new webpack.HotModuleReplacementPlugin()//热加载插件    -----------即便不用这个插件，改变css，js，页面依旧会自动刷新？
+        // new webpack.HotModuleReplacementPlugin(),//热加载插件    -----------即便不用这个插件，改变css，js，页面依旧会自动刷新？
+        // new webpack.optimize.OccurrenceOrderPlugin(),//为组件分配ID，通过这个插件webpack可以分析和优先考虑使用最多的模块，并为它们分配最小的ID
+        // new webpack.optimize.UglifyJsPlugin(),//webpack.optimize.UglifyJsPlugin has been removed, please use config.optimization.minimize instead.
+        //个人认为：随着webpack版本的提升，压缩代码功能早已经变成了默认设置
+
+        // new ExtractTextPlugin("style.css"),//暂时无效,而且对于4.1版本的webpack而言会影响打包-------
+        new CleanWebpackPlugin('dist/*.*', {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        })
     ]
 }
