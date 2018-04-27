51dfc14fc4f9ce2c7a48dc5c143d80032bf3ae52
Mr.Gao <gaofangshang9868@163.com>
2018-04-27 16:16:34 +0800

    s

--- element-ui/src/router/index.js
+++ element-ui/src/router/index.js
@@ -3,20 +3,24 @@ import Router from 'vue-router'
 
 //首页和其他路由
 const Home = resolve => require(['views/Home'], resolve);
+const test = resolve => require(['views/Test'], resolve);
+
+
 Vue.use(Router);
 
 const router =  new Router({
   routes: [
       { path: '/', redirect:'home' },
-      { path: '/home', component: Home ,meta:{ level: 1 }},
+      { path: '/home', component: Home },
+      { path: '/test',component:test}
   ]
 })
 router.beforeEach((to, from, next) => {
   //token验证功能,token过期则跳回登陆界面
   if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (!getToken()) {
-      router.push("/login");
-    }
+    // if (!getToken()) {
+    //   router.push("/login");
+    // }
   }
   next();
 });
