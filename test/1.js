eccc39193232d3cd8c1ed96839c0d3a60e863a69
Mr.Gao <gaofangshang9868@163.com>
2018-07-27 18:10:40 +0800

    save

---
+++ test/1.js
@@ -0,0 +1,12 @@
+{
+    readonly:!t.editable || t.readonly,
+    disabled:t.disabled,
+    size:t.size,
+    title:t.title,
+    placeholder:t.placeholder,
+    value:t.visualValue,
+    onpaste:"return false",
+    showClear:!1,
+    readonly:"",
+    icon:t.iconType
+}
