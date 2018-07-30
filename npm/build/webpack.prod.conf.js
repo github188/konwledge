31cf54170bfa8d7f6dccfeef876623792d530ef7
Mr.Gao <gaofangshang9868@163.com>
2018-07-30 17:12:18 +0800

    删除多余配置

--- npm/build/webpack.prod.conf.js
+++ npm/build/webpack.prod.conf.js
@@ -2,11 +2,21 @@ const path = require('path')
 const webpack = require('webpack');
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+function resolve (dir) {
+    return path.join(__dirname, '..', dir)
+}
 
 
 const webpackConfig = merge(baseWebpackConfig, {
     plugins: [
         new webpack.BannerPlugin('版权所有，翻版必究'),
+        new CleanWebpackPlugin(resolve('dist/*.*'), {
+            root: resolve("/"),
+            verbose: true,
+            dry: false
+        })
     ]
 })
 
